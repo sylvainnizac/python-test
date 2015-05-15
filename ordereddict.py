98bfa0f15714518d3ff98d79dc6a2614e7729a2b
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-05-15 14:20:18 +0200

    ajout listes dans __Init__

--- ordereddict.py
+++ ordereddict.py
@@ -2,3 +2,5 @@ class OrderedDict:
 """This aim of this class is to create a sortable dict. The object must work like a dictionnary AND like a list."""
 
   def __Init__():
+    keyslist = []
+    valueslist = []
