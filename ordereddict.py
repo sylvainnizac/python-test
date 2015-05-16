e79aa51e028fa39065e45ce952fa5b8e6487557d
sylvainnizac <sylvain.nizac@laposte.net>
2015-05-16 13:24:56 +0200

    ajout des methodes speciales __getitem__ et __setitem__

--- ordereddict.py
+++ ordereddict.py
@@ -1,6 +1,40 @@
 class OrderedDict:
-"""This aim of this class is to create a sortable dict. The object must work like a dictionnary AND like a list."""
+    """
+    This aim of this class is to create a sortable dict.
+    The object must work like a dictionnary AND like a list.
+    """
 
-  def __Init__():
-    keyslist = []
-    valueslist = []
+    def __init__(self, *entree):
+        """
+        the constructor creates le empty dictolist and, if needed, load the input data
+        """
+        self.keyslist = []
+        self.valueslist = []
+
+        if len(entree) != 0: #if there is parameters
+            for data in entree: #for each parameter
+                if type(data) is dict: #if the parameter is a dict
+                    for keys in data: #for each key in the dict
+                        self.keyslist.append(keys) #append data keys to keyslist
+                        self.valueslist.append(data[keys]) #append data values to valueslist
+
+    def __getitem__(self, entree):
+        """
+        OrderedDict[entree]
+        """
+        try:
+            temp = self.keyslist.index(entree)
+            return self.valueslist[temp]
+        except:
+            return "Cette clé n\'existe pas"
+
+    def __setitem__(self, entree, valeur):
+        """
+        OrderedDict[entree] = valeur
+        """
+        if entree not in self.keyslist:
+            self.keyslist.append(entree)
+            self.valueslist.append(valeur)
+        else:
+            temp = self.keyslist.index(entree)
+            self.valueslist[temp] = valeur
